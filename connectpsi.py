a459f170b96c73ec29dfffe3f16e76eb303696b9
Christopher <christopher.hecker@tum.de>
2021-09-25 10:49:44 +0200

    Added board and quantum list

--- connectpsi.py
+++ connectpsi.py
@@ -3,8 +3,18 @@
 import numpy as np
 import pyglet as pg
 
+
 def main():
-    pass
+    """
+    Main method to be run when executing file.
+    board: array functioning as the game board
+    quantum_list: list of quantum stones
+    """
+    width, height = 7, 6
+    player_nr = 2
+    board = np.ndarray((height, width))
+    quantum_list = []
+
 
 if __name__ == main():
     main()
