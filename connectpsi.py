890ad7f8245bba7f12eb17879edc23be80cdef02
Sarah <sarah.fritsche@my-enet.de>
2021-09-25 11:49:50 +0200

    check row and column

--- connectpsi.py
+++ connectpsi.py
@@ -18,6 +18,73 @@ player_nr = 2
 draw_counter = 0
 board = np.zeros((height, width))
 quantum_list = []
+psi = 4                            #number of connected pieces to win
 
 if __name__ == main():
     main()
+
+
+def check_win() -> int:
+    winner = -1
+    winturn = 2 * width * height  # a number sufficiently big, that the real winturn is smaller for sure
+    tempwinturn = -1
+    player = -1
+    counter = 0
+    # check rows
+    for row in range(height):
+        for column in range(width):
+            field = board[row][column]
+
+            if (field != 0 and field not in quantum_list):         # If  a classical piece lies there
+                new_player = field % player_nr
+                if (player == new_player):
+                    counter += 1
+                    if (field > tempwinturn):
+                        tempwinturn = field
+
+                    if (counter == psi and tempwinturn < winturn):  # If we have a new winner
+                        winturn = tempwinturn
+                        winner = player
+
+                else:
+                    player = new_player
+                    counter = 1
+                    tempwinturn = field
+            else:
+                counter = 0
+                player = -1
+                tempwinturn = -1
+
+            counter = 0
+            tempwinturn = -1
+
+    # check columns
+    for column in range(width):
+        for row in range(height):
+
+            field = board[row][column]
+
+            if (field != 0 and field not in quantum_list):  # If  a classical piece lies there
+                new_player = field % player_nr
+                if (player == new_player):
+                    counter += 1
+                    if (field > tempwinturn):
+                        tempwinturn = field
+
+                    if (counter == psi and tempwinturn < winturn):  # if we have a new winner
+                        winturn = tempwinturn
+                        winner = player
+
+                else:
+                    player = new_player
+                    counter = 1
+                    tempwinturn = field
+            else:
+                counter = 0
+                player = -1
+                tempwinturn = -1
+
+            counter = 0
+            tempwinturn = -1
+
+    return -1
