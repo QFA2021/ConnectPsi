f32cfc6cf7531ba0f9ce3f5a032c6ebd546b0faa
Christopher <christopher.hecker@tum.de>
2021-09-25 10:50:45 +0200

    Fixed global variables

--- connectpsi.py
+++ connectpsi.py
@@ -10,11 +10,12 @@ def main():
     board: array functioning as the game board
     quantum_list: list of quantum stones
     """
+    pass
+
+
+if __name__ == main():
     width, height = 7, 6
     player_nr = 2
     board = np.ndarray((height, width))
     quantum_list = []
-
-
-if __name__ == main():
     main()
