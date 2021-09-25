033da981b68a797181f0ba1955f254281b5807b2
Christopher <christopher.hecker@tum.de>
2021-09-25 15:03:49 +0200

    Updated doc strings

--- connectpsi.py
+++ connectpsi.py
@@ -7,8 +7,8 @@ import random
 
 def main():
     """Main method to be run when executing file.
-    board: array functioning as the game board
-    quantum_list: list of quantum stones
+    :board: array functioning as the game board
+    :quantum_list: list of quantum stones
     """
     pass
 
